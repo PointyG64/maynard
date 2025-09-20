2bbfc30fc72f56f885c108aab1756f21d0be77f6
PointyG64 <tom.maynard2002@gmail.com>
2025-09-20 01:04:22 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,10 +4,10 @@ const nextConfig = {
   output: 'export',
   distDir: '../public',
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Maynards Registry',
+    description: 'My list of Kasm Workspaces',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
+    listUrl: 'https://pointyg64.github.io/kasm-registry',
     contactUrl: 'https://kasmweb.com/support',
   },
   reactStrictMode: true,
